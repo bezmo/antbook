d169ab45f54a7e5521ae335bbd5d578e4ccdbc18
bezmo <sthmoy@gmail.com>
2021-04-05 22:11:52 +0900

    add greedy algorithm script

---
+++ scripts/greedy_algorithm.py
@@ -0,0 +1,23 @@
+"""貪欲法
+"""
+
+
+def greedy_algorithm():
+    s = 610
+    medal_list = [500, 100, 50, 10, 5, 1]
+    medal_cnt = [2, 0, 3, 1, 2, 3]
+
+    cnt = 0
+    for i in range(len(medal_list)):
+        x = int(s / medal_list[i])
+        cnt += x if x <= medal_cnt[i] else medal_cnt[i]
+        s -= medal_list[i] * (x if x < medal_cnt[i] else medal_cnt[i])
+
+        if s == 0:
+            break
+
+    print(cnt)
+
+
+if __name__ == '__main__':
+    greedy_algorithm()
